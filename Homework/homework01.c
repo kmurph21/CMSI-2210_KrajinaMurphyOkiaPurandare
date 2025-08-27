516c7e5268d138b3cc0ff9fa28a502876fdffd9b
Kaitlin <kmurph58@lion.lmu.edu>
2025-08-27 15:02:16 -0700

    Update and rename homework01 to homework01.c

---
+++ Homework/homework01.c
@@ -0,0 +1,10 @@
+
+#include <studio.h>
+#include <stdlib.h>
+
+int main(int argc, char*argv[]) {
+  char input[25];
+  printf("What is your name: ");
+  scanf("%s", input);
+  printf("\n Hello, %s\n", input);
+}
