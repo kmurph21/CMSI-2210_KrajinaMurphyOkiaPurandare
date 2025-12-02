be4f5d71674ff218a264dc2828fdeeae1334b093
Kaitlin <kmurph58@lion.lmu.edu>
2025-12-02 13:36:35 -0800

    Create dec2bin.c

---
+++ Homework/homework05/dec2bin.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stlib.h>
+int main(int argc, char *argv[]) {
+  unisgned int num;
+  if (argc==2) {
+    num=(unsigned int)atoi(argv[1]);
+  }
+  else {
+    printf("Enter a base 10 number:");
+    scanf("%u", &num);
+  }
+  char binary[33];
+  binary[32]='\0';
+  for(int i=31; i>=0; i--) {
+    binary[i]=num(num&1)?'1':'0';
+    num>>=1;
+  }
+  printf("%s\n", binary);
+  return 0;
+}
