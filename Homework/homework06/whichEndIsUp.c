4735adfb30e5da0d01e78332a5d28b17796b7665
danielleokia <117638522+danielleokia@users.noreply.github.com>
2025-12-03 15:37:04 -0800

    Add documentation for endianness program
    Added a comment block explaining the purpose of the program.

--- Homework/homework06/whichEndIsUp.c
+++ Homework/homework06/whichEndIsUp.c
@@ -1,1 +1,6 @@
-
+/*
+In networking, it is necessary to make sure that bytes are transimitted in the proper order so the receiving device can interpret them 
+correctly. This requires that big-endian and little-endian values are converted to something called Network Byte Order before they are 
+transmitted. For this problem, write a C program whichEndIsUp.c that will determine whether your computer is big-endian or little-endian 
+as was discussed in class.
+*/
